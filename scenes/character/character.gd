478794990f7506d6993c5506b5f6e6592b526ece
zhtsu <zhtsu@qq.com>
2024-06-05 17:53:57 +0800

    Test ParallaxBackground

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -1,11 +1,27 @@
-extends Node2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
+var gravity : Variant = ProjectSettings.get_setting("physics/2d/default_gravity")
+var movement_speed : float = 600.0
+var character_mass : float = 40
+
+@onready var character_sprite : AnimatedSprite2D = get_node("AnimatedSprite2D")
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+
+	if not is_on_floor():
+		velocity.y += gravity * character_mass * delta
+
+	if Input.is_action_pressed("test_right") and is_on_floor():
+		velocity.x += movement_speed
+		character_sprite.flip_h = false
+	elif Input.is_action_pressed("test_left") and is_on_floor():
+		velocity.x -= movement_speed
+		character_sprite.flip_h = true
+
+	move_and_slide()

4ceaaa3a1a2147400ef0d2bcb5461020e0b16adf
zhtsu <zhtsu@qq.com>
2024-06-10 18:20:56 +0800

    Player double jump is configurable

--- scenes/character/player/player.gd
+++ scenes/character/player/player.gd
@@ -7,11 +7,11 @@ enum EPlayerState
 	Walk,
 	Run,
 	Jump,
+	DoubleJump,
 	Fall,
 	Idle,
 	Attack,
 	Cheer,
-	DoubleJump,
 	Kick,
 	Shove,
 	Ready
@@ -23,16 +23,17 @@ enum EPlayerState
 @export var jump_fall_time : float = 0.5
 @export var jump_height : float = 2.0
 @export var jump_distance : float = 4.0
+@export var can_double_jump : bool = false
 
 @onready var anim_tree : AnimationTree = $"AnimationTree"
 
+var player_jump_position : Vector2
+var has_double_jump : bool = false
 var jump_gravity : float
 var jump_velocity : float
 var fall_gravity : float
 var movement_speed : float
 var player_state : EPlayerState = EPlayerState.Run
-var has_double_jump : bool = false
-
 
 
 func _ready() -> void:
@@ -43,15 +44,20 @@ func _physics_process(delta: float) -> void:
 	position.x = 120.0
 
 	if velocity.y > 0.0:
-		velocity.y += jump_gravity * delta
+		velocity.y += fall_gravity * delta
 		if not is_on_floor() &&\
-		(player_state == EPlayerState.Jump || player_state == EPlayerState.DoubleJump):
+		(player_state == EPlayerState.Jump ||\
+		 player_state == EPlayerState.Run):
 			set_fall_state()
 	else:
-		velocity.y += fall_gravity * delta
+		velocity.y += jump_gravity * delta
+		if player_state == EPlayerState.Jump && is_on_floor():
+			if player_jump_position.y - position.y > 100.0:
+				reset_jump_state()
 	
 	if is_on_floor():
-		if player_state == EPlayerState.Fall:
+		if player_state == EPlayerState.Fall ||\
+		   player_state == EPlayerState.DoubleJump:
 			reset_jump_state()
 
 	move_and_slide()
@@ -74,6 +80,9 @@ func set_run_state() -> void:
 func set_jump_state() -> void:
 	player_state = EPlayerState.Jump
 
+func set_double_jump_state() -> void:
+	player_state = EPlayerState.DoubleJump
+
 func set_fall_state() -> void:
 	player_state = EPlayerState.Fall
 
@@ -85,10 +94,6 @@ func set_attack_state() -> void:
 
 func set_cheer_state() -> void:
 	player_state = EPlayerState.Cheer
-
-func set_double_jump_state() -> void:
-	player_state = EPlayerState.DoubleJump
-	has_double_jump = true
 
 func set_kick_state() -> void:
 	player_state = EPlayerState.Kick
@@ -103,15 +108,20 @@ func set_ready_state() -> void:
 func jump() -> void:
 	if player_state == EPlayerState.Run:
 		set_jump_state()
+		player_jump_position = position
 		velocity.y = jump_velocity
 
 
 func double_jump() -> void:
 	set_double_jump_state()
 	velocity.y = jump_velocity
+	has_double_jump = true
 
 
 func is_can_double_jump() -> bool:
+	if can_double_jump == false:
+		return false
+
 	if player_state == EPlayerState.Fall && has_double_jump == false:
 		return true
 

861d571bc774aa733ba89291f315e803b1f94999
zhtsu <zhtsu@qq.com>
2024-07-24 18:00:38 +0800

    Add MapThemeItem

--- scenes/ui/main_ui.gd
+++ scenes/ui/main_ui.gd
@@ -6,11 +6,9 @@ var starter_showed : bool = true
 var ranklist_showed : bool = false
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass

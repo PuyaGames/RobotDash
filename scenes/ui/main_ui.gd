aedf6526ac8d48386ee10fd18f0d8c840e172802
zhtsu <zhtsu@qq.com>
2024-07-25 17:59:08 +0800

    Update map theme name

--- scenes/ui/main_ui.gd
+++ scenes/ui/main_ui.gd
@@ -1,14 +1,19 @@
 extends CanvasLayer
 
 
+@onready var map_theme_bar: VBoxContainer = $PickMapTheme/VBoxContainer/ScrollContainer/VBoxContainer
+
 var settings_showed : bool = false
 var starter_showed : bool = true
 var ranklist_showed : bool = false
+var pick_map_theme_showed : bool = false
 
 
 func _ready() -> void:
-	pass # Replace with function body.
+	var tscn_map_theme_item : PackedScene = load("res://scenes/ui/map_theme_item.tscn")
+	for map_type : Enums.EMapType in Enums.EMapType.values():
+		var map_theme_item : MapThemeItem = tscn_map_theme_item.instantiate()
+		map_theme_item.type = map_type
+		map_theme_bar.add_child(map_theme_item)
 
 
-func _process(delta: float) -> void:
-	pass
--- scenes/ui/map_theme_item.gd
+++ scenes/ui/map_theme_item.gd
@@ -1,9 +1,16 @@
 @tool
 extends Button
+class_name MapThemeItem
 
 
-@export var label : String = "主题名称"
-@export var type : Enums.EMapType = Enums.EMapType.Halloween_Green
+@export var type : Enums.EMapType = Enums.EMapType.Halloween_Green:
+	set(new_value):
+		type = new_value
+		var map_theme_data : MapThemeData = theme_data_dict[type]
+		$TextureRect.texture = load(map_theme_data.tex_path)
+		$Label.text = map_theme_data.label
+		
+var locked : bool = true
 
 class MapThemeData:
 	var label : String
@@ -13,12 +20,54 @@ class MapThemeData:
 		label = in_label
 		tex_path = in_tex_path
 
-var tex_dict : Dictionary = {
+var theme_data_dict : Dictionary = {
+	Enums.EMapType.Halloween_Green :
+		MapThemeData.new("古堡", "res://resources/ui/map_theme/halloween_01.atlastex"),
+	Enums.EMapType.Halloween_Red :
+		MapThemeData.new("血色滋生", "res://resources/ui/map_theme/halloween_02.atlastex"),
 	Enums.EMapType.Halloween_Blue :
-		MapThemeData.new("万圣节（一）", "res://resources/ui/map_theme/halloween_01.atlastex")
+		MapThemeData.new("墓地", "res://resources/ui/map_theme/halloween_03.atlastex"),
+	Enums.EMapType.Halloween_Orange :
+		MapThemeData.new("万圣节", "res://resources/ui/map_theme/halloween_04.atlastex"),
+	Enums.EMapType.Desert_Cactus :
+		MapThemeData.new("风化", "res://resources/ui/map_theme/desert_01.atlastex"),
+	Enums.EMapType.Desert_Rock :
+		MapThemeData.new("仙人掌", "res://resources/ui/map_theme/desert_02.atlastex"),
+	Enums.EMapType.Desert_Sky :
+		MapThemeData.new("怪石嶙峋", "res://resources/ui/map_theme/desert_03.atlastex"),
+	Enums.EMapType.Desert_Dusk :
+		MapThemeData.new("常规沙漠", "res://resources/ui/map_theme/desert_04.atlastex"),
+	Enums.EMapType.Sweet_Pink :
+		MapThemeData.new("超级大蛋糕", "res://resources/ui/map_theme/sweet_01.atlastex"),
+	Enums.EMapType.Sweet_Blue :
+		MapThemeData.new("云朵", "res://resources/ui/map_theme/sweet_02.atlastex"),
+	Enums.EMapType.Sweet_Green :
+		MapThemeData.new("童话中的森林", "res://resources/ui/map_theme/sweet_03.atlastex"),
+	Enums.EMapType.Sweet_Cyan :
+		MapThemeData.new("魔法的城堡", "res://resources/ui/map_theme/sweet_04.atlastex"),
+	Enums.EMapType.Beach_Blue :
+		MapThemeData.new("高饱和海滩", "res://resources/ui/map_theme/beach_01.atlastex"),
+	Enums.EMapType.Beach_Green :
+		MapThemeData.new("满地贝壳", "res://resources/ui/map_theme/beach_02.atlastex"),
+	Enums.EMapType.Beach_Cyan :
+		MapThemeData.new("海边的酒吧", "res://resources/ui/map_theme/beach_03.atlastex"),
+	Enums.EMapType.Beach_Dusk :
+		MapThemeData.new("黄昏与海", "res://resources/ui/map_theme/beach_04.atlastex")
 }
 
 
+func set_selected(selected : bool) -> void:
+	if selected:
+		$Selected.show()
+	else:
+		$Selected.hide()
+		
+		
 func _ready() -> void:
-	pass # Replace with function body.
+	if locked:
+		disabled = true
+		$Locked.show()
 
+
+func _on_button_button_down() -> void:
+	pass # Replace with function body.
